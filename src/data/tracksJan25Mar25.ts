ff47c944e0bd2a3e1e9c6b478826389c789b011b
Diane Shan <dianeshan40785@gmail.com>
2025-02-10 21:51:22 -0800

    Update office hours jan/feb

--- src/data/tracksJan25Mar25.ts
+++ src/data/tracksJan25Mar25.ts
@@ -1,6 +1,10 @@
 // audio files Jan 2025 - Mar 2025
 import keyelements from "./CCE20250106_寻找新工作MP3.mp3";
 import diffopinions from "./CCE20250113_兩個老闆MP3.mp3";
+import startingpoint from "./CCE20250120_向理想的工作邁進MP3.mp3";
+import righttrack from "./CCE20250127_是不是在對的跑道.mp3";
+import growingnetwork from "./CCE20250203_人際網絡.mp3";
+import meetingpatrons from "./CCE20250210_遇見職場中的貴人.mp3";
 
 export const tracks11 = [
   {
@@ -16,4 +20,28 @@ export const tracks11 = [
     date: "01/13/2025",
     info: "Q1: 我有兩個老闆(权柄), 他們的意見是衝突的, 我該聼誰的?  I have two bosses (authorities) and their opinions conflict. Who should I listen to? Q2: 我該滿足哪個人的需要？我可以怎麽做? Whose needs should I meet? What can I do?",
   },
+  {
+    title: "還沒找到一個起步點, 讓我向理想的工作邁進, 我該放棄嗎? I haven't found a starting point that will allow me to move towards my ideal job. Should I give up?",
+    src: startingpoint,
+    date: "01/20/2025",
+    info: "1.我喜歡藝術，有一些創作天賦，想做導演或策展人，但這兩個職位都需要資源和人脈，如果要進藝術行業或影視行業，我會面臨著例如：入行機會少、工作強度大、價值觀和工作職責有衝突等問題，對我來說，這是不是一條合乎神心意的道路，如果是，我該怎麼邏輯閉合地去行動？有哪些重點是我需要把握的？ Shall I pursue a film or show director's job? 2.我在實習時看到了一些人格優秀、學識淵博的同事，我非常欣賞他們，也想成為那樣的人，我想如果能留在實習的公司，繼續和優秀的人共事，自己或許也會變成這樣的人，這種職涯規劃的邏輯是正確的嗎？ Does qualities of my coworker matter? 3. 目前我還在找全職工作，因為這幾年的求學和實習，我累積了一些債務，父親給我聯繫了一份工作, 它和我想在一二線城市從事藝術工作的職業規劃沒有關係，入職時間是年後，如果我在年前，還沒找到可以cover 生活成本和債務的工作，就要做一個決定( 參加三四月份的春招, 或回去工作).我想請老師們幫我分析一下這兩條路各自的利弊，或者還有別的路可以走，但因為負債，我可能已經在思考上陷入了盲區，我還是希望能做一個有利於長期規劃的短期內的決定。 Shall I keep looking for opportunities to pursue my dream job? Or shall I pick a job that covers my cost of living & paying off debt?",
+  },
+  {
+    title: "怎麼知道自己是不是在對的跑道上？How do you know if you are on the right track?",
+    src: righttrack,
+    date: "01/27/2025",
+    info: "工作要邁向第六年，感覺還是依附在別人？沒法完全獨立，獨立保險經紀人這工作到底是我擅長的嗎？ 案例問答 Example Q&A Q1: 哪些部分是你無法獨立操作的？ A1: 仔細想想，其實好像沒有哪部份不能獨立操作，但是會怕客人問我稅務問題（我沒有稅務背景）我無法回答，再加上經驗不夠多，會緊張怕表現不好，如何開發自己的市場也是挑戰。 Q2: 你的主管有何回饋？ (1)哪些方面你超越了他/她的預期？ (2)哪些方面你沒有達到他/她的期望？ A(1)服務客戶細心，做事可靠，會察言觀色，主動完成交待任務。 A(2)太在意，怕犯錯，以至於不敢放手做、想太多，很多事情都會先問他（老闆），但老闆覺得，有些事我自己決定就好，但我摸不著他的界線。他希望我能製作視頻，但是我的產出效能很差，花的時間不成比例。 Q3: 你的同事有何回饋？ (1)哪些方面你超越了他/她的預期？ (2)哪些方面你沒有達到他/她的期望？ A(1) 口調清楚，專業型象佳，親和力強，討人喜歡 A(2) 各家保險公司產品細節部份可再加強 Q4: 你自己檢視有何發現？ (1)哪些方面你超越了你的預期？ (2)哪些方面不如你的期望？ A(1)花很多時間精力在工作上，老闆要求的會立即完成，沒要求的也會想要怎麼去做會更好 A(2)當自己努力沒受到肯定或結果不如預期時，硬知識常常記完又忘，很沮喪，雖然信主但還是會內耗，沒自信，覺得自己好像處處不如人。老二哲學，相當優秀的副手，卻不想當領袖。"
+  },
+  {
+    title: "擴展人際網絡 Growing Your Network",
+    src: growingnetwork,
+    date: "02/03/2025",
+    info: "Q1: 我需要擴展人際網絡嗎? 爲什麽? 如何做?  Do I need to expand my network? Why? How to do it? Q2: 許多我不認識的人在 LinkedIn 上對我發出連結邀請，我該怎麼辦？A lot of unknown people asked to connect with me on my LinkedIn, what should I do with them?"
+  },
+  {
+    title: "遇見職場中的貴人 Meeting patron(s) for your career",
+    src: meetingpatrons,
+    date: "02/10/2025",
+    info: "Q1: 職場中有所謂的「貴人」嗎？如何判斷一個人是在幫助我還是在利用我？Is there a so-called patron in the workplace? How do I tell if someone is helping me or taking advantage of me? Q2: 朋友推薦的兼職，也要簽訂兼職合同，這是合理的嗎，這種要求過分嗎？Is it reasonable to sign a part-time contract for a part-time job recommended by a friend? Is this too much?"
+  }
 ];
