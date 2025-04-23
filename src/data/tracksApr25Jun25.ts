5866f97b0288697d30e3688da2e5fbf447fe7519
Diane Shan <dianeshan40785@gmail.com>
2025-04-23 16:48:04 -0700

    Update office hours

--- src/data/tracksApr25Jun25.ts
+++ src/data/tracksApr25Jun25.ts
@@ -1,5 +1,7 @@
 // audio files Apr 2025 - June 2025
 import setbackatwork from "./CCE20250407_面對處理失敗MP3.mp3";
+import continuework from "./CCE20250414_是否繼續目前工作MP3.mp3";
+import nogossip from "./CCE20250421_不想説閒話MP3.mp3";
 
 export const tracks12 = [
   {
@@ -8,4 +10,16 @@ export const tracks12 = [
     date: "04/07/2025",
     info: "Q1: 我該如何看待工作上的失敗/挫折? How should I see failure/setbacks at work? Q2. 我從工作上的失敗/挫折得到什麽益處? What do I gain from failure/frustration at work?"
   },
+  {
+    title: "下一步方向: 是否繼續目前工作 Next step: whether to continue the current work",
+    src: continuework,
+    date: "04/14/2025",
+    info: "Q1 現在的工作我也已經禱告幾年不知道要不要換，這是我畢業後的第一份工作，神把我安排在這個跟我性格完全不符的銷售崗位，我覺得壓力有點大，神要磨練我，但是怎麽樣才能知道天父爸爸對我的心意呢？I have been praying about my current job for several years and I don’t know whether I should change it. This is my first job after graduation. God has placed me in this sales position which is totally incompatible with my personality. I feel a bit stressed and God wants to temper me. But how can I know the will of my Heavenly Father for me?"
+  },
+  {
+    title: "我不想説閒話，但也不想得罪人，我該怎麽辦? I don't want to gossip, but I also don't want to offend anyone. What should I do?",
+    src: nogossip,
+    date: "04/21/2025",
+    info: "Q1: 我廠長的老婆總是跟我講論別⼈。我是神的兒女,不能跟她⼀起講別⼈的不好。但我不接她的話，她從此就不再跟我說話。我該怎麼辦？ My factory manager's wife always talks to me about other people. I am a child of God and cannot talk about other people's bad things with her. But I don't respond to her and she stops talking to me. What should I do?"
+  }
 ]
